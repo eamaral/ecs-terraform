4656dad0a35881a04cb6d4dad6f7308864df913f
Erik <erik.fernandes87@gmail.com>
2025-03-30 12:39:13 -0300

    workflow dispatch

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "ecs_cluster_name" {
 output "ecs_service_name" {
   value = aws_ecs_service.fastfood_service.name
 }
+
+output "ecs_service_sg_id" {
+  description = "ID do Security Group do ECS que acessa o banco"
+  value       = aws_security_group.ecs_service_sg.id
+}
